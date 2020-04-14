bf107c997997115182e7d9723b3c28b1f29966b7
shervheimb6 <shervheimb6@gmail.com>
2020-04-14 15:09:55 -0500

    android studio app

--- app/src/main/java/com/example/myapplication5/MainActivity.java
+++ app/src/main/java/com/example/myapplication5/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.myapplication5;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-
+import com.zopim.android.sdk.api.ChatApi;
 import com.zopim.android.sdk.api.ZopimChat;
 import com.zopim.android.sdk.prechat.ZopimChatActivity;
 
